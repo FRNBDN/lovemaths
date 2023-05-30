4841739ef6773e785d3c6d58c763c8a1e19855ee
FRNBDN <francesco.badini94@gmail.com>
2023-05-30 14:17:15 +0900

    fix addition question display correct values

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
  * The main application "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runApp() {
+function runApp(operatorType) {
   // Creates two random numbers between 1 and 25
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
@@ -38,9 +38,9 @@ function calculateCorrectAnswer() {}
 
 function incrementScore() {}
 
-function incrementWrongAnswer(operand1, operand2) {}
+function incrementWrongAnswer() {}
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
   document.getElementById("operand1").textContent = operand1;
   document.getElementById("operand2").textContent = operand2;
   document.getElementById("operator").textContent = "+";
