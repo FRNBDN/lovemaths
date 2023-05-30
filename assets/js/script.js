8ef2e00f2a5459eac7f239f9eaadedaf667569c5
FRNBDN <francesco.badini94@gmail.com>
2023-05-30 15:30:28 +0900

    add code for division operator

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,6 +39,8 @@ function runApp(operatorType) {
     displayMultiplyQuestion(num1, num2);
   } else if (operatorType === "subtract") {
     displaySubtractQuestion(num1, num2);
+  } else if (operatorType === "division") {
+    displayDivisionQuestion(num1, num2);
   } else {
     alert(`Unknown operator type: ${operatorType}`);
     throw `Unknown operator type: ${operatorType}. Aborting!`;
@@ -80,6 +82,8 @@ function calculateCorrectAnswer() {
     return [operand1 * operand2, "multiply"];
   } else if (operator === "-") {
     return [operand1 - operand2, "subtract"];
+  } else if (operator === "/") {
+    return [operand1 / operand2, "division"];
   } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
@@ -120,4 +124,11 @@ function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById("operand1").textContent = operand1;
   document.getElementById("operand2").textContent = operand2;
   document.getElementById("operator").textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+  operand1 = operand1 * operand2;
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "/";
 }
