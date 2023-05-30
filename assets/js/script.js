5b83a8575c3a640658b2b370f88b5eab54f52ddd
FRNBDN <francesco.badini94@gmail.com>
2023-05-30 14:37:54 +0900

    add code for subtraction operand

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,8 @@ function runApp(operatorType) {
     displayAdditionQuestion(num1, num2);
   } else if (operatorType === "multiply") {
     displayMultiplyQuestion(num1, num2);
+  } else if (operatorType === "subtract") {
+    displaySubtractQuestion(num1, num2);
   } else {
     alert(`Unknown operator type: ${operatorType}`);
     throw `Unknown operator type: ${operatorType}. Aborting!`;
@@ -69,6 +71,8 @@ function calculateCorrectAnswer() {
     return [operand1 + operand2, "addition"];
   } else if (operator === "x") {
     return [operand1 * operand2, "multiply"];
+  } else if (operator === "-") {
+    return [operand1 - operand2, "subtract"];
   } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
@@ -97,7 +101,13 @@ function displayAdditionQuestion(operand1, operand2) {
   document.getElementById("operator").textContent = "+";
 }
 
-function displaySubtractQuestion() {}
+function displaySubtractQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent =
+    operand1 > operand2 ? operand1 : operand2;
+  document.getElementById("operand2").textContent =
+    operand1 > operand2 ? operand2 : operand1;
+  document.getElementById("operator").textContent = "-";
+}
 
 function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById("operand1").textContent = operand1;
