3cc67748c41e4ce9ab915f7835ef82977f3d34c7
FRNBDN <francesco.badini94@gmail.com>
2023-05-30 14:12:25 +0900

    update app to start on addition by default

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function() {
+    button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
                 alert("Clicked Submit!");
             } else {
-                let gameType = this.getAttribute("data-type");
-                alert(`Clicked ${gameType}`);
+        let operatorType = this.getAttribute("data-type");
+        runApp(operatorType);
             }
         });
     }
+
+  runApp("addition");
 });
 
 
