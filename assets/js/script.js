631c1ffd3c3277a577943d1699a8a5dd760bae5f
FRNBDN <francesco.badini94@gmail.com>
2023-05-30 14:34:57 +0900

    add code for multiplication operator

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,8 @@ function runApp(operatorType) {
 
   if (operatorType === "addition") {
     displayAdditionQuestion(num1, num2);
+  } else if (operatorType === "multiply") {
+    displayMultiplyQuestion(num1, num2);
   } else {
     alert(`Unknown operator type: ${operatorType}`);
     throw `Unknown operator type: ${operatorType}. Aborting!`;
@@ -65,6 +67,8 @@ function calculateCorrectAnswer() {
 
   if (operator === "+") {
     return [operand1 + operand2, "addition"];
+  } else if (operator === "x") {
+    return [operand1 * operand2, "multiply"];
   } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
@@ -95,4 +99,8 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion() {}
 
-function displayMultiplyQuestion() {}
+function displayMultiplyQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "x";
+}
